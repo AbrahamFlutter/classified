efc1ab8a37bc07943be434a6ac7368811f996d2c
AbrahamFlutter <abraham.magana@itksquare.edu.mx>
2022-03-30 12:54:48 -0600

    03_display_circle_image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//task3
 
 void main() {
   runApp(
@@ -9,9 +10,12 @@ void main() {
           width: double.infinity,
           color: Color.fromARGB(255, 7, 132, 248),
           child: Center(
-            child: Image.network(
-              "https://media-exp1.licdn.com/dms/image/C4D03AQGtAetUGzvj_Q/profile-displayphoto-shrink_200_200/0/1549850502850?e=1653523200&v=beta&t=kGzPrxz3623IGqmPjiolV8P7hRbGm2X9Nn1yZ4Ahp4k",
-              width: 200,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.network(
+                "https://media-exp1.licdn.com/dms/image/C4D03AQGtAetUGzvj_Q/profile-displayphoto-shrink_200_200/0/1549850502850?e=1653523200&v=beta&t=kGzPrxz3623IGqmPjiolV8P7hRbGm2X9Nn1yZ4Ahp4k",
+                width: 200,
+              ),
             ),
           ),
         ),
