936e7961ae5aa65e0ce45cc5b0d410b84a8884ed
AbrahamFlutter <abraham.magana@itksquare.edu.mx>
2022-03-30 12:46:27 -0600

    02_display_image

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ void main() {
         body: Container(
           height: double.infinity,
           width: double.infinity,
-          color: Color.fromARGB(255, 97, 185, 14),
-          child: const Center(
-            child: Text(
-              "Hello Flutter!",
-              style: TextStyle(fontSize: 40, color: Colors.white),
+          color: Color.fromARGB(255, 7, 132, 248),
+          child: Center(
+            child: Image.network(
+              "https://media-exp1.licdn.com/dms/image/C4D03AQGtAetUGzvj_Q/profile-displayphoto-shrink_200_200/0/1549850502850?e=1653523200&v=beta&t=kGzPrxz3623IGqmPjiolV8P7hRbGm2X9Nn1yZ4Ahp4k",
+              width: 200,
             ),
           ),
         ),
@@ -19,10 +19,3 @@ void main() {
     ),
   );
 }
-//create new branch
-
-//git branch branch_name
-//git checkout  branch_name 
-//git add . 
-//git commit -m "branch_name"
-//git push --set-upstream origin branch_name
